b377cdf32436c330710c84c439ef0f1dce0e6741
xavier1 <selinkesler@gmail.com>
2021-07-19 14:43:47 +0200

    pose est

--- run_ehpi_for_data_preprocessing.py
+++ run_ehpi_for_data_preprocessing.py
@@ -63,16 +63,16 @@ if __name__ == '__main__':
     # out = cv2.VideoWriter('/Users/sk82620/Pictures/Camera Roll/chair.mp4', fourcc, 20.0, (1280, 720))
     
     # Pose Network
-    pose_model = pose_resnet.get_pose_net(pose_resnet_config)
-    logger.info('=> loading model from {}'.format(pose_resnet_config.model_state_file))
-    pose_model.load_state_dict(torch.load(pose_resnet_config.model_state_file))
-    pose_model = pose_model.cuda()
-    pose_model.eval()
+    # pose_model = pose_resnet.get_pose_net(pose_resnet_config)
+    # logger.info('=> loading model from {}'.format(pose_resnet_config.model_state_file))
+    # pose_model.load_state_dict(torch.load(pose_resnet_config.model_state_file))
+    # pose_model = pose_model.cuda()
+    # pose_model.eval()
 
-    with open('/home/xavier1/catkin_ws/src/gui/src/networks/pose_estimation_2d_nets/human_pose.json', 'r') as f:
+    with open('./networks/pose_estimation_2d_nets/human_pose.json', 'r') as f:
         human_pose = json.load(f)
 
-    pose_net = Pose2DNetResnet(pose_model, skeleton_type,human_pose)
+    pose_net = Pose2DNetResnet(skeleton_type,human_pose)
     pose_tracker = PoseTracker(image_size=image_size, skeleton_type=skeleton_type)  
     
 
